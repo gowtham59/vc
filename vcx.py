48d91ec14fc74c850942a820922bbd75cb7909b9
gowtham59 <47033355+gowtham59@users.noreply.github.com>
2019-07-02 17:23:07 +0530

    Update vcx.py

--- vcx.py
+++ vcx.py
@@ -1,17 +1,17 @@
-aas = int(input())
-bbs = list(map(int,input().split()))
-ccs = []
-ee = []
-for i in enumerate(bbs):
-  ccs.append(i)
-for i in range(0,len(bbs)):
-  for j in range(0,len(bbs)):
-    for k in range(0,len(bbs)):
-      if(ccs[i][1] < ccs[j][1] < ccs[k][1]):
-        if(ccs[i][0] < ccs[j][0] < ccs[k][0]):
-          d = [ccs[i][1],ccs[j][1],ccs[k][1]]
-          ee.append(d)
-if(len(ee) != 0):
-  print(len(ee))
+a1s = int(input())
+b2s = list(map(int,input().split()))
+c3s = []
+e4 = []
+for i in enumerate(b2s):
+  c3s.append(i)
+for i in range(0,len(b2s)):
+  for j in range(0,len(b2s)):
+    for k in range(0,len(b2s)):
+      if(c3s[i][1] < c3s[j][1] < c3s[k][1]):
+        if(c3s[i][0] < c3s[j][0] < c3s[k][0]):
+          d = [c3s[i][1],c3s[j][1],c3s[k][1]]
+          e4.append(d)
+if(len(e4) != 0):
+  print(len(e4))
 else:
   print("0")
